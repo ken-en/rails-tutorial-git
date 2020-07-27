fa68700b32cf1eb4c971c04fb312e25d61937be2
EC2 Default User <ec2-user@ip-172-31-60-205.ec2.internal>
2020-07-27 03:51:31 +0000

    update at 20200727

--- sample/hello_app/app/controllers/application_controller.rb
+++ sample/hello_app/app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-  include SessionHelper
+  include SessionsHelper
 
   def hello
     render html: "hello, world!"
   end
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+
 end
--- sample/hello_app/config/routes.rb
+++ sample/hello_app/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
  root 'toppages#index'
+ 
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+ 
+ get 'signup', to: 'users#new'
+ resources :users, only: [:index, :new, :create]
  get "hello" => "hello#view" #追加
 end
 
