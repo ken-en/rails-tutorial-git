303f8944a8f2602ae9dd9efb1926075d5fead728
EC2 Default User <ec2-user@ip-172-31-60-205.ec2.internal>
2020-08-02 13:13:25 +0000

    update at 1

--- sample/hello_app/app/controllers/business_statues_controller.rb
+++ sample/hello_app/app/controllers/business_statues_controller.rb
@@ -1,50 +1,50 @@
-class Business_statuesController < ApplicationController
+class BusinessStatuesController < ApplicationController
 
-  def index
-    @business_statues = Business_statue.all
+  def index 
+    @business_statues = BusinessStatue.all
   end
   
-  def show
+  def show 
     @business_statue = Business_statue.find(params[:id])
   end
 
-  def new
+  def new 
     @business_statue = Business_statue.new
   end
 
-  def create
+  def create 
     @business_statue = Business_statue.new(business_statue_params)
 
     if @business_statue.save
-      flash[:success] = ‘Business_statue が正常に投稿されました'
+      flash[:success] = 'Business_statue が正常に投稿されました'
       redirect_to @business_statue
     else
-      flash.now[:danger] = ‘Business_statue が投稿されませんでした'
+      flash.now[:danger] = 'Business_statue が投稿されませんでした'
       render :new
     end
   end
 
-  def edit
+  def edit 
     @business_statue = Business_statue.find(params[:id])
   end
   
-  def update
+  def update 
     @business_statue = Business_statue.find(params[:id])
 
     if @business_statue.update(business_statue_params)
-      flash[:success] = ‘Business_statue は正常に更新されました'
+      flash[:success] = 'Business_statue は正常に更新されました'
       redirect_to @business_statue
     else
-      flash.now[:danger] = ‘Business_statue は更新されませんでした'
+      flash.now[:danger] = 'Business_statue は更新されませんでした'
       render :edit
     end
   end
   
-  def destroy
+  def destroy 
     @business_statue = Business_statue.find(params[:id])
     @business_statue.destroy
 
-    flash[:success] = ‘Business_statue は正常に削除されました'
+    flash[:success] = 'Business_statue は正常に削除されました'
     redirect_to business_statues_url
   end
   
